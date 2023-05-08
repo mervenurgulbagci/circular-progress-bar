170dce572b7ed8e34e06562906296b1934e42627
mervenurgulbagci <mervenurgulbagci@gmail.com>
2023-05-08 11:01:54 +0300

    Circular animated progress bar

--- CircularProgressBar/ViewController.swift
+++ CircularProgressBar/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var circularProgressBarView: CircularProgressBarView!
+    var circularViewDuration: TimeInterval = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpCircularProgressBarView()
     }
-
+    
+    func setUpCircularProgressBarView() {
+        // set view
+        circularProgressBarView = CircularProgressBarView(frame: .zero)
+        // align to the center of the screen
+        circularProgressBarView.center = view.center
+        // call the animation with circularViewDuration
+        circularProgressBarView.progressAnimation(duration: circularViewDuration)
+        // add this view to the view controller
+        view.addSubview(circularProgressBarView)
+    }
 
 }
 
